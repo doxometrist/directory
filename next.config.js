340ad3c400e6a9e7224983c738c149127d86878a
Doxometrist <doxometrist@protonmail.com>
2023-06-21 09:31:04 +0100

    trying the deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: "/directory",
+  images: {
+    unoptimized: true
+  },
   experimental: {
     serverActions: true
   }
